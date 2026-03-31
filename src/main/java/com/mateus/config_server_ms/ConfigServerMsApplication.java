e9041b3a94698295e6d5db43c1e4102a9ba78a0b
mateus <mateus.magalhaes@hotmail.com.br>
2026-03-31 10:34:30 -0300

    config repository

---
+++ src/main/java/com/mateus/config_server_ms/ConfigServerMsApplication.java
@@ -0,0 +1,15 @@
+package com.mateus.config_server_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateus/config_server_ms/ConfigServerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.config_server_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
